c54db4abc998481e521ccb24fbf3ba34c7739e6d
Daemons.Hellenistic <truc.bugcreator666@gmail.com>
2019-10-16 23:32:07 +0700

    _add api route for HomeController _add Login api _remove unnecessary usings

--- eMentorUserServices/Controllers/HomeController.cs
+++ eMentorUserServices/Controllers/HomeController.cs
@@ -1,10 +1,16 @@
-﻿using eMentor.Controllers;
+﻿using eMentor.Common.Models;
+using eMentor.Controllers;
 using eMentor.DBContext.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
 
 namespace eMentorUserServices.Controllers
 {
+    [Route("api/[controller]")]
     public class HomeController : BaseController
     {
         private readonly ITokenManager _tokenManager;
@@ -15,6 +21,30 @@ namespace eMentorUserServices.Controllers
             ITokenManager tokenManager) : base(loggerService, environment, userService)
         {
             _tokenManager = tokenManager;
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login([FromBody] LoginModel user)
+        {
+            if (user == null || string.IsNullOrWhiteSpace(user.PassCode)) return GetBadRequestResult(ErrorMessageCode.FIELDS_IS_EMPTY);
+
+            try
+            {
+                var response = await _userService.LoginUser(user);
+
+                if (response != null && response.Data != null)
+                {
+                    return GetOKResult(response.Data);
+                }
+                else
+                {
+                    return GetResult(response);
+                }
+            }
+            catch (Exception ex)
+            {
+                return GetServerErrorResult(ex.ToString());
+            }
         }
     }
 }
